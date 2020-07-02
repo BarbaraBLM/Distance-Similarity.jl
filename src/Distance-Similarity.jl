c7a1d8b72ba9943549b9b690a31e86ec31546775
BarbaraBLM <47249285+BarbaraBLM@users.noreply.github.com>
2020-07-02 10:23:30 -0300

    Update Distance-Similarity.jl
    Specifying the type of arguments and adding string docs

--- src/Distance-Similarity.jl
+++ src/Distance-Similarity.jl
@@ -7,8 +7,18 @@ export minkowski_dist
 export weighted_minkowski_dist
 export jaccard_similarity
 
+"""
+	euclidean_dist(u::AbstractArray, v::AbstractArray)
 
-function euclidean_dist(u, v)
+Returns the euclidean distance between the two arrays
+
+# Examples
+```julia-repl
+julia> euclidean_dist([1.4, 2], [2.8, 1])
+1.7204650534085253
+```
+"""
+function euclidean_dist(u::AbstractArray, v::AbstractArray)
 	if(length(u) != length(v))
 		throw(ArgumentError("Vectors must be the same size."))
 		return -1
@@ -17,8 +27,18 @@ function euclidean_dist(u, v)
 	
 end
 
+"""
+	manhattan_dist(u::AbstractArray, v::AbstractArray)
 
-function manhattan_dist(u, v)
+Returns the manhattan or cityblock distance between the two arrays
+
+# Examples
+```julia-repl
+julia> manhattan_dist([1.4, 2], [2.8, 1])
+2.4
+```
+"""
+function manhattan_dist(u::AbstractArray, v::AbstractArray)
 	if(length(u) != length(v))
 		throw(ArgumentError("Vectors must be the same size."))
 		return -1
@@ -26,8 +46,18 @@ function manhattan_dist(u, v)
 	sum(abs.(u - v))
 end
 
+"""
+	cosine_similarity(u::AbstractArray, v::AbstractArray)
 
-function cosine_similarity(u, v)
+Returns the cosine similarity between the two arrays.
+
+# Examples
+```julia-repl
+julia> cosine_similarity([1.4, 2], [2.8, 1])
+0.8155910368616317
+```
+"""
+function cosine_similarity(u::AbstractArray, v::AbstractArray)
 	if(length(u) != length(v))
 		throw(ArgumentError("Vectors must be the same size."))
 		return -1
@@ -36,7 +66,20 @@ function cosine_similarity(u, v)
 
 end
 
-function minkowski_dist(u, v, p)
+"""
+	minkowski_dist(u::AbstractArray, v::AbstractArray, p::Int)
+
+Returns the Minkowski distance between the two arrays
+
+Note: p must be greater than or equal to 1
+
+# Examples
+```julia-repl
+julia> minkowski_dist([1.4, 2], [2.8, 1], 2)
+1.7204650534085253
+```
+"""
+function minkowski_dist(u::AbstractArray, v::AbstractArray, p::Int)
 	if(length(u) != length(v))
 		throw(ArgumentError("Vectors must be the same size."))
 		return -1
@@ -49,8 +92,20 @@ function minkowski_dist(u, v, p)
 	
 end
 
+"""
+	weighted_minkowski_dist(u::AbstractArray, v::AbstractArray, w::AbstractArray, p::Int)
 
-function weighted_minkowski_dist(u, v, w, p)
+Returns the Minkowski distance between the two arrays given a certain weight for each pair of data in u and v.
+
+Note: p must be greater than or equal to 1
+
+# Examples
+```julia-repl
+julia> weighted_minkowski_dist([1.4, 2], [2.8, 1], [2.0, 1.0], 2)
+2.973213749463701
+```
+"""
+function weighted_minkowski_dist(u::AbstractArray, v::AbstractArray, w::AbstractArray, p::Int)
 	if((length(u) != length(v)) || (length(u) != length(w)) || (length(v) != length(w)))
 		throw(ArgumentError("Vectors must be the same size."))
 		return -1
@@ -63,12 +118,23 @@ function weighted_minkowski_dist(u, v, w, p)
 	return ((sum((w.*(abs.(u .- v))).^p)))^(1/p)
 end
 
-function jaccard_similarity(u, v)
+"""
+	jaccard_similarity(u::AbstractArray, v::AbstractArray)
+
+Returns the jaccard similarity between the two arrays.
+
+# Examples
+```julia-repl
+julia> jaccard_similarity([1.4, 2], [2.8, 1])
+0.0
+```
+"""
+function jaccard_similarity(u::AbstractArray, v::AbstractArray)
 	if(length(u) != length(v))
 		throw(ArgumentError("Vectors must be the same size."))
 		return -1
 	end
-	return length(intersect(u, v)) / length(union(u, v))
+	return length(intersect((u::AbstractArray, v::AbstractArray))) / length(union((u::AbstractArray, v::AbstractArray)))
 end
 
 
